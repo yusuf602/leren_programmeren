9bf2e802e806ede299a9cffdf786c32f9e0d19db
yusuf602 <99082369@mydavinci.nl>
2024-09-09 11:13:28 +0200

    Create m1_o1.py

---
+++ modules 1/m1_o1.py
@@ -0,0 +1,17 @@
+prijs_croissantjes = 0.39
+aantal_croissanjes = 17
+
+prijs_stokbrood = 2.78
+aantal_stokbrood = 2
+
+korting_bon = 0.50
+aantal_kortingsbonnen = 3 
+
+totaal_croissant = prijs_croissantjes * aantal_croissanjes
+totaal_stokbrood = prijs_stokbrood * aantal_stokbrood
+
+totaal_korting = korting_bon * aantal_kortingsbonnen
+
+totaal_tebetalen = (totaal_croissant + totaal_stokbrood) - totaal_korting
+
+print(f"De totale te betalen bedrag voor de feestlunch is {totaal_tebetalen} euro.")
