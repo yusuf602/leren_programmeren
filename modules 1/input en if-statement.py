85410917a7adea390492c7e27117434663ad6a98
yusuf602 <99082369@mydavinci.nl>
2024-09-16 11:27:41 +0200

    naam

---
+++ modules 1/input en if-statement.py
@@ -0,0 +1,14 @@
+a = int(input('Kies een getal: '))
+b = int(input('kies nog een getal: '))
+max = a
+min = a
+
+if a > b:
+    max = a
+    print(f"a is het grootste getal: {max}")
+elif b > a:
+    min = a
+    print(f"a is het kleinste getal: {min}")
+    
+
+        
