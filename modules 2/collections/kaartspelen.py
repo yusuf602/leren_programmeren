a75dbc3c244db2bee544add3ff3dff91b96d6d09
yusuf602 <99082369@mydavinci.nl>
2025-01-15 09:46:50 +0100

    kaartspel
    haal 7 kaarten uit dek en laat de overgebleven kaarten zien

--- modules 2/collections/kaartspelen.py
+++ modules 2/collections/kaartspelen.py
@@ -1,6 +1,17 @@
-deck = ("harten:", "klaveren", "schoppen", "ruiten")
+import random
 
-aantal=13
+# Deck genereren
+kleuren = ["Harten", "Klaveren", "Schoppen", "Ruiten"]
+waarden = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Boer", "Vrouw", "Heer", "Aas"]
+deck = [f"{waarde} van {kleur}" for kleur in kleuren for waarde in waarden] + ["Joker", "Joker"]
 
-for i in range(2-11):
-    print(deck[i], aantal)
+random.shuffle(deck)
+getrokken_kaarten = deck[:7]
+overgebleven_kaarten = deck[7:]
+
+# Resultaat 
+print(f"Getrokken kaarten ({len(getrokken_kaarten)}): {getrokken_kaarten}")
+for i, kaart in enumerate(getrokken_kaarten, start=1):
+    print(f"{i} kaart: {kaart}")
+print('')
+print(f"Overgebleven kaarten ({len(overgebleven_kaarten)}): {overgebleven_kaarten}")
