419479f6b0223068b4208af68fd5563d5bf436df
yusuf602 <99082369@mydavinci.nl>
2025-01-30 09:48:45 +0100

    ROBOTARM

---
+++ modules 2/proef/robotarm/expert2.py
@@ -0,0 +1,7 @@
+from RobotArm import RobotArm
+
+# Import the challenges (in this case challenges/example.py)
+from challenges.expert import challenges
+
+# load the robotarm with a challenge on a level (max 3)
+robotArm = RobotArm(challenges[2],0)
