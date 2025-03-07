cf6ab8a33078b0d88cffeb096b567e66ea187e3e
yusuf602 <99082369@mydavinci.nl>
2025-03-07 10:26:51 +0100

    realisatie aanpassingen

--- modules 3/proef module 3/realisatie.py
+++ modules 3/proef module 3/realisatie.py
@@ -1,32 +1,44 @@
 import random
 
+
 deelnemers = []
+
 while len(deelnemers) < 3:
-    naam = input ('voer een unieke naam in: ').lower()
+    naam = input("Voer een unieke naam in: ").strip().lower()
     if naam in deelnemers:
-        print('deze naam is al in gebruik, probeer een andere naam.')
-    else:
+        print("Deze naam is al in gebruik, probeer een andere.")
+    elif naam:
         deelnemers.append(naam)
+
 while True:
     keuze = input("Wil je nog een naam toevoegen? (ja/nee): ").strip().lower()
     if keuze == "ja":
-        naam = input("Voer een unieke naam in: ").strip()
+        naam = input("Voer een unieke naam in: ").strip().lower()
         if naam in deelnemers:
             print("Deze naam is al ingevoerd, probeer een andere.")
         elif naam:
             deelnemers.append(naam)
-        else:
-            print("Ongeldige invoer, probeer opnieuw.")
     elif keuze == "nee":
         break
     else:
         print("Ongeldige keuze, typ 'ja' of 'nee'.")
+    
 lootjes = deelnemers[:]  
-random.shuffle(lootjes)  
+while True:
+    random.shuffle(lootjes)
+    if all(deelnemers[i] != lootjes[i] for i in range(len(deelnemers))):
+        break 
 
-while any(deelnemers[i] == lootjes[i] for i in range(len(deelnemers))):  
-    random.shuffle(lootjes)  
-
-for i in range(len(deelnemers)):  
-    print(f"{deelnemers[i]} heeft {lootjes[i]} als lootje getrokken.")  
+verdeling = {deelnemers[i]: lootjes[i] for i in range(len(deelnemers))}
+    
+print("Alle lootjes zijn getrokken! Voer je naam in om te zien wie je hebt.")
+    
+while True:
+    naam = input("Voer je naam in (of typ 'stop' om te stoppen): ").strip().lower()
+    if naam == "stop":
+        break
+    elif naam in verdeling:
+        print(f"{naam}, jij hebt {verdeling[naam]} getrokken!")
+    else:
+        print("Deze naam zit niet in de lijst. Probeer opnieuw.")
 
