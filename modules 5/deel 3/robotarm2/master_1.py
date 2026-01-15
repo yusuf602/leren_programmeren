202b377cf3da37653349e24986956c0f4546c687
yusuf602 <99082369@mydavinci.nl>
2026-01-15 10:18:38 +0100

    master
    dit zijn de eerste 2 opdrachten van robotarm hoger niveau. gebruik gemaatk van listen om kleuren op te slaan en hhier van gebruik te maken om kleuren te onderschieden van elkaar

---
+++ modules 5/deel 3/robotarm2/master_1.py
@@ -0,0 +1,37 @@
+from RobotArm import RobotArm
+from master import *
+
+robotArm = RobotArm(challenges[1], 0)
+
+kleuren = []
+aantal_blokken = 9   # op jouw screenshot staan er 9
+
+# FASE 1: alles scannen
+for i in range(aantal_blokken):
+    robotArm.grab()
+    kleur = robotArm.scan()
+    kleuren.append(kleur)
+    robotArm.drop()
+    robotArm.moveRight()
+
+# aparte kleur vinden
+for kleur in kleuren:
+    if kleuren.count(kleur) == 1:
+        aparte_kleur = kleur
+        break
+
+# terug naar het begin
+for i in range(aantal_blokken):
+    robotArm.moveLeft()
+
+# FASE 2: aparte kleur pakken
+for i in range(aantal_blokken):
+    robotArm.grab()
+    if robotArm.scan() == aparte_kleur:
+        robotArm.grab()
+        break
+    robotArm.drop()
+    robotArm.moveRight()
+
+robotArm.report()
+robotArm.help()
---
+++ modules 5/deel 3/robotarm2/master_2.py
@@ -0,0 +1,129 @@
+from RobotArm import RobotArm
+from master import *
+
+robotArm = RobotArm(challenges[2], 0)
+
+kleuren = []
+for i in range(4):
+    robotArm.moveRight()
+for i in range(5):
+    robotArm.grab()
+    kleur = robotArm.scan()
+    kleuren.append(kleur)
+    robotArm.drop()
+    robotArm.moveRight()
+robotArm.grab()
+kleur = robotArm.scan()
+kleuren.append(kleur)
+robotArm.drop()
+
+kleur1 = kleuren[0]
+kleur2 = None
+kleur3 = None
+
+aantal1 = 0
+aantal2 = 0
+aantal3 = 0
+
+for kleur in kleuren:
+    if kleur == kleur1:
+        aantal1 += 1
+    elif kleur2 is None:
+        kleur2 = kleur
+        aantal2 += 1
+    elif kleur == kleur2:
+        aantal2 += 1
+    else:
+        kleur3 = kleur
+        aantal3 += 1
+
+
+if aantal1 == 3:
+    eerst = kleur1
+elif aantal2 == 3:
+    eerst = kleur2
+else:
+    eerst = kleur3
+
+if aantal1 == 2:
+    tweede = kleur1
+elif aantal2 == 2:
+    tweede = kleur2
+else:
+    tweede = kleur3
+
+if aantal1 == 1:
+    derde = kleur1
+elif aantal2 == 1:
+    derde = kleur2
+else:
+    derde = kleur3
+
+
+for i in range(5):
+    robotArm.moveLeft()
+
+
+stapel_vakken = {
+    eerst: 1,   
+    tweede: 2,  
+    derde: 3    
+}
+
+positie_arm = 1
+
+stapel_vakken = {
+    eerst: 1,   
+    tweede: 2, 
+    derde: 3    
+}
+
+def ga_naar(vak):
+    global positie_arm
+    while positie_arm < vak:
+        robotArm.moveRight()
+        positie_arm += 1
+    while positie_arm > vak:
+        robotArm.moveLeft()
+        positie_arm -= 1
+
+def stapel_blokken(kleur, aantal):
+    global positie_arm
+    gevonden = 0
+    for i in range(10, 0, -1):
+        ga_naar(i)        
+        robotArm.grab()
+        if robotArm.scan() == kleur:
+            ga_naar(stapel_vakken[kleur])
+            robotArm.drop()
+            gevonden += 1
+            if gevonden == aantal:
+                break
+        else:
+            robotArm.drop()
+
+stapel_blokken(eerst, 3)
+stapel_blokken(tweede, 2)
+stapel_blokken(derde, 1)
+
+
+
+volgende_vak = 1 
+
+
+positie_arm = 1
+
+blokken_vakjes = kleuren  
+
+# your code ends here
+
+# report the results of the mission
+robotArm.report()
+
+# want help? Unlock code below!
+robotArm.help()
+
+# want to inspect a solution? Unlock code below!
+# robotArm.showSolution()
+# robotArm.wait()
+
