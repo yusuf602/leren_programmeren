f535b7e28b88f6ebae6db9c3ebc273bcab57a666
yusuf602 <99082369@mydavinci.nl>
2026-03-10 10:20:10 +0100

    Update lingo.py
    hier is te zien dat ik een fout heb verbeterd. je ziet dat mijn eerste print fout was. als ik een woord als begin kreeg stond er niet bij hoelang het woord was. dit is veranderd en vebeterd. ernaast heb je nu ook 5 pgiingen inplaats van oneindig

--- meesterproef/lingo.py
+++ meesterproef/lingo.py
@@ -1,9 +1,18 @@
 import random
 from lingowords import words
 
+pogingen = 5
+
 gekozen_woord =  random.choice(words)
 
 huidige_status = gekozen_woord[0] + "_" * (len(gekozen_woord)-1)
 
-print("beginletter", gekozen_woord[0])
 print (huidige_status)
+for pogingen in range(pogingen):
+    gok = input("doe een gok: ")
+
+    if gok == gekozen_woord:
+        print(f"goed geraden het woord was {gekozen_woord}")
+        break
+    else:
+        print("helaas fout")
